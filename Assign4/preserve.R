4a5ac792b687a6a92b2c04b51eaf40a581106281
Ben Parli <bparli@gmail.com>
2016-04-19 20:42:17 -0700

    final updates

--- Assign4/preserve.R
+++ Assign4/preserve.R
@@ -3,6 +3,7 @@ source('./ReserveDesign_R/binvec2dec.r')
 source('./ReserveDesign_R/getstate.r')
 source('./ReserveDesign_R/dec2binvec.r')
 source('./ReserveDesign_R/mdp_example_reserve.r')
+source('./ReserveDesign_R/explore_solution_reserve.r')
 library(MDPtoolbox)
 
 set.seed(1)
@@ -13,7 +14,7 @@ PR <- mdp_example_reserve(M, 0.2)
 P <- PR$P
 R <- PR$R
 
-specs.val = mdp_value_iteration(P=P,R=R,discount=0.96, epsilon=0.01, max_iter = 1000)
+specs.val = mdp_value_iteration(P=P,R=R,discount=0.96, epsilon=0.001, max_iter = 1000)
 specs.pol = mdp_policy_iteration(P=P,R=R,discount=0.96, max_iter = 1000, policy0 = rep(0,2187))
 
 specs.Q = mdp_Q_learning(P=P, R=R, discount=0.96, N=1000000)
@@ -21,3 +22,8 @@ specs.Q = mdp_Q_learning(P=P, R=R, discount=0.96, N=1000000)
 specs.val.eval = mdp_eval_policy_TD_0(P=P, R=R, discount=0.9, policy=specs.val$policy)
 specs.pol.eval = mdp_eval_policy_TD_0(P=P, R=R, discount=0.9, policy=specs.pol$policy)
 specs.Q.eval = mdp_eval_policy_TD_0(P=P, R=R, discount=0.9, policy=specs.Q$policy)
+
+# Explore solution
+explore_solution_reserve(numeric(7), specs.val$policy, M, P, R)
+explore_solution_reserve(numeric(7), specs.pol$policy, M, P, R)
+explore_solution_reserve(numeric(7), specs.Q$policy, M, P, R)
